2e18088792cef1a21183ca5f57384a2b593b5981
Weidaicheng <wdcdavyc@outlook.com>
2017-11-23 11:51:42 +0800

    独立消息重复性验证

--- Weixin.Netcore.Core/Message/MenuClickMessageHandler.cs
+++ Weixin.Netcore.Core/Message/MenuClickMessageHandler.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Weixin.Netcore.Cache;
-using Weixin.Netcore.Model.WeixinMessage;
+﻿using Weixin.Netcore.Model.WeixinMessage;
 using Weixin.Netcore.Utility;
 
 namespace Weixin.Netcore.Core.Message
@@ -10,14 +6,14 @@ namespace Weixin.Netcore.Core.Message
     /// <summary>
     /// 菜单按钮点击消息处理
     /// </summary>
-    public class MenuClickMessageHandler : IClickEventHandler, IRepetValid
+    public class MenuClickMessageHandler : IClickEventHandler
     {
-        private readonly ICache _cache;
+        private readonly MessageRepetHandler _messageRepetHandler;
         private readonly IMessageReceive<ClickEvtMessage> _messageReceive;
 
-        public MenuClickMessageHandler(ICache cache, IMessageReceive<ClickEvtMessage> messageReceive) 
+        public MenuClickMessageHandler(MessageRepetHandler messageRepetHandler, IMessageReceive<ClickEvtMessage> messageReceive) 
         {
-            _cache = cache;
+            _messageRepetHandler = messageRepetHandler;
             _messageReceive = messageReceive;
         }
 
@@ -26,7 +22,7 @@ namespace Weixin.Netcore.Core.Message
             ClickEvtMessage receiveMsd = _messageReceive.GetEntity(xml);
 
             //消息重复
-            if (!MessageRepetValid(receiveMsd.FromUserName + receiveMsd.CreateTime))
+            if (!_messageRepetHandler.MessageRepetValid(receiveMsd.FromUserName + receiveMsd.CreateTime))
             {
                 return "success";
             }
@@ -40,19 +36,6 @@ namespace Weixin.Netcore.Core.Message
             };
 
             return responseMsg.CreateXml();
-        }
-
-        public bool MessageRepetValid(string key)
-        {
-            var value = _cache.Get(key);
-
-            if (string.IsNullOrEmpty(value))
-            {
-                _cache.Set(key, key, TimeSpan.FromSeconds(5));
-                return true;
-            }
-
-            return false;
         }
     }
 }
---
+++ Weixin.Netcore.Core/Message/MessageRepetHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Weixin.Netcore.Cache;
+
+namespace Weixin.Netcore.Core.Message
+{
+    /// <summary>
+    /// 消息重复性处理
+    /// </summary>
+    public class MessageRepetHandler
+    {
+        private readonly ICache _cache;
+
+        public MessageRepetHandler(ICache cache)
+        {
+            _cache = cache;
+        }
+
+        /// <summary>
+        /// 消息重复性验证
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns>
+        /// true：消息未重复
+        /// false：消息重复
+        /// </returns>
+        public bool MessageRepetValid(string key)
+        {
+            var value = _cache.Get(key);
+
+            if (string.IsNullOrEmpty(value))
+            {
+                _cache.Set(key, key, TimeSpan.FromSeconds(5));
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
