aa3aec0308334bb9c95a7a3af0a1181089e338ef
Weidaicheng <wdcdavyc@outlook.com>
2017-11-26 09:35:40 +0800

    创建菜单测试

---
+++ Weixin.Netcore.Test/MenuTest.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Weixin.Netcore.Core.InterfaceCaller;
+
+namespace Weixin.Netcore.Test
+{
+    [TestClass]
+     public class MenuTest
+    {
+        [TestMethod]
+        public void CreateMenuTest()
+        {
+            OAuthInterfaceCaller oAuthInterface = new OAuthInterfaceCaller(new RestClient());
+            var accessToken = oAuthInterface.GetAccessToken("wx6eff55d0d76e210f", "60ab768429e8fc6b86abaa9cfd1c6565");
+            string menu = @"
+ {
+     ""button"":[
+     {
+                ""type"":""click"",
+          ""name"":""今日歌曲"",
+          ""key"":""V1001_TODAY_MUSIC""
+      },
+      {
+                ""name"":""菜单"",
+           ""sub_button"":[
+           {	
+               ""type"":""view"",
+               ""name"":""搜索"",
+               ""url"":""http://www.soso.com/""
+            },
+            {
+               ""type"":""click"",
+               ""name"":""赞一下我们"",
+               ""key"":""V1001_GOOD""
+            }]
+       }]
+ }
+";
+            MenuInterfaceCaller menuInterfaceCaller = new MenuInterfaceCaller(new RestClient());
+            Console.WriteLine(menuInterfaceCaller.CreateMenu(accessToken.access_token, menu));
+        }
+    }
+}
