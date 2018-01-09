ef26f2a9429e9e22797adecff358b66836aec0a5
Weidaicheng <wdcdavyc@outlook.com>
2018-01-09 16:02:25 +0800

    fix

--- Weixin.Netcore.Test/MessageTest.cs
+++ Weixin.Netcore.Test/MessageTest.cs
@@ -34,7 +34,7 @@ namespace Weixin.Netcore.Core.Test
             IMessageReply<TextMessage> messageReply = new TextMessageReply();
             ClickEvtMessageHandlerBase clickEvtMessageHandler = new ClickEventReplyTextHandler(messageReply);
             IMessageRepetValidUsage messageRepetValidUsage = new MessageRepetValidUsage(true);
-            MessageProcesser processer = new MessageProcesser(messageRepetHandler, messageRepetValidUsage, new TextMessageHandlerBase(), new ImageMessageHandlerBase(), new VoiceMessageHandlerBase(), new VideoMessageHandlerBase(), new ShortVideoMessageHandlerBase(), new LocationMessageHandlerBase(), new LinkMessageHandlerBase(), new SubscribeEvtMessageHandlerBase(), new UnsubscribeEvtMessageHandlerBase(), new ScanEvtMessageHandlerBase(), new LocationEvtMessageHandlerBase(), clickEvtMessageHandler);
+            MessageProcesser processer = new MessageProcesser(messageRepetHandler, messageRepetValidUsage, new TextMessageHandlerBase(), new ImageMessageHandlerBase(), new VoiceMessageHandlerBase(), new VideoMessageHandlerBase(), new ShortVideoMessageHandlerBase(), new LocationMessageHandlerBase(), new LinkMessageHandlerBase(), new SubscribeEvtMessageHandlerBase(), new UnsubscribeEvtMessageHandlerBase(), new ScanEvtMessageHandlerBase(), new LocationEvtMessageHandlerBase(), clickEvtMessageHandler, new ScanSubscribeEvtMessageHandlerBase());
             Console.WriteLine(processer.ProcessMessage(message));
         }
 
