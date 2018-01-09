c7ebcf3dda8e821de4e52daa816ead0a0da225c0
Weidaicheng <wdcdavyc@outlook.com>
2018-01-09 20:29:15 +0800

    添加经纬度计算位置帮助方法

--- Weixin.Netcore.Utility/UtilityHelper.cs
+++ Weixin.Netcore.Utility/UtilityHelper.cs
@@ -133,5 +133,61 @@ namespace Weixin.Netcore.Utility
             int nonce = random.Next(100000000, 999999999);
             return nonce.ToString();
         }
+
+        #region 距离计算
+        #region private
+        private static double EARTH_RADIUS = 6371.0;//km 地球半径 平均值，千米
+
+        private static double HaverSin(double theta)
+        {
+            var v = Math.Sin(theta / 2);
+            return v * v;
+        }
+
+        /// <summary>
+        /// 将角度换算为弧度。
+        /// </summary>
+        /// <param name="degrees">角度</param>
+        /// <returns>弧度</returns>
+        private static double ConvertDegreesToRadians(double degrees)
+        {
+            return degrees * Math.PI / 180;
+        }
+
+        private static double ConvertRadiansToDegrees(double radian)
+        {
+            return radian * 180.0 / Math.PI;
+        }
+        #endregion
+
+        /// <summary>
+        /// 给定的经度1，纬度1；经度2，纬度2. 计算2个经纬度之间的距离。
+        /// </summary>
+        /// <param name="lon1">经度1</param>
+        /// <param name="lat1">纬度1</param>
+        /// <param name="lon2">经度2</param>
+        /// <param name="lat2">纬度2</param>
+        /// <returns>距离（公里、千米）</returns>
+        public static double Distance(double lon1, double lat1, double lon2, double lat2)
+        {
+            //用haversine公式计算球面两点间的距离。
+            //经纬度转换成弧度
+            lat1 = ConvertDegreesToRadians(lat1);
+            lon1 = ConvertDegreesToRadians(lon1);
+            lat2 = ConvertDegreesToRadians(lat2);
+            lon2 = ConvertDegreesToRadians(lon2);
+
+            //差值
+            var vLon = Math.Abs(lon1 - lon2);
+            var vLat = Math.Abs(lat1 - lat2);
+
+            //h is the great circle distance in radians, great circle就是一个球体上的切面，它的圆心即是球心的一个周长最大的圆。
+            var h = HaverSin(vLat) + Math.Cos(lat1) * Math.Cos(lat2) * HaverSin(vLon);
+
+            var distance = 2 * EARTH_RADIUS * Math.Asin(Math.Sqrt(h));
+
+            return distance;
+        }
+        #endregion
     }
 }
