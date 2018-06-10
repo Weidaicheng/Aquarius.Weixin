ee7c6717535e68643da4f75480dbb088ead8d5a0
Weidaicheng <wdcdavyc@outlook.com>
2018-06-10 13:09:59 +0800

    修改DI扩展
    修改AddAquariusWeixin方法，添加AquariusWeixinBuilder

---
+++ src/Aquarius.Weixin/Aquarius.Weixin/Core/Configuration/DependencyInjection/AquariusWeixinBuilder.cs
@@ -0,0 +1,160 @@
+﻿using Aquarius.Weixin.Cache;
+using Aquarius.Weixin.Core.Authentication;
+using Aquarius.Weixin.Core.Configuration.DependencyInjection.Options;
+using Aquarius.Weixin.Core.Exceptions;
+using Aquarius.Weixin.Core.InterfaceCaller;
+using Aquarius.Weixin.Core.JsApi;
+using Aquarius.Weixin.Core.MaintainContainer;
+using Aquarius.Weixin.Core.Message;
+using Aquarius.Weixin.Core.Message.Handler;
+using Aquarius.Weixin.Core.Message.Handler.DefaultHandler;
+using Aquarius.Weixin.Core.Message.Processer;
+using Aquarius.Weixin.Core.Message.Reply;
+using Aquarius.Weixin.Core.Middleware;
+using Aquarius.Weixin.Entity.Configuration;
+using Aquarius.Weixin.Entity.Enums;
+using Aquarius.Weixin.Entity.WeixinMessage;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using RestSharp;
+using System;
+
+namespace Aquarius.Weixin.Core.Configuration.DependencyInjection
+{
+    public class AquariusWeixinBuilder : IAquariusWeixinBuilder
+    {
+        private readonly IServiceCollection _services;
+        private readonly IServiceProvider _provider;
+        private readonly AquariusWeixinOptions _options;
+
+        public AquariusWeixinBuilder(IServiceCollection services)
+        {
+            _services = services;
+            _provider = services.BuildServiceProvider();//get an instance of IServiceProvider
+            _options = _provider.GetRequiredService<IOptions<AquariusWeixinOptions>>().Value;//resolve an instance of AquariusWeixinOptions
+        }
+
+        public void AddRestSharp()
+        {
+            _services.AddScoped<IRestClient, RestClient>();
+        }
+
+        public void AddInterfaceCallers()
+        {
+            _services.AddScoped<DisposableMessageInterfaceCaller, DisposableMessageInterfaceCaller>();
+            _services.AddScoped<MenuInterfaceCaller, MenuInterfaceCaller>();
+            _services.AddScoped<OAuthInterfaceCaller, OAuthInterfaceCaller>();
+            _services.AddScoped<TemplateMessageInterfaceCaller, TemplateMessageInterfaceCaller>();
+            _services.AddScoped<TicketInterfaceCaller, TicketInterfaceCaller>();
+            _services.AddScoped<UserTagManageInterfaceCaller, UserTagManageInterfaceCaller>();
+            _services.AddScoped<WxPayInterfaceCaller, WxPayInterfaceCaller>();
+        }
+
+        public void AddContainers()
+        {
+            _services.AddScoped<AccessTokenContainer, AccessTokenContainer>();
+            _services.AddScoped<AuthorizationContainer, AuthorizationContainer>();
+            _services.AddScoped<TicketContainer, TicketContainer>();
+        }
+
+        public void AddSignGenerAndVerifyer()
+        {
+            _services.AddScoped<SignatureGenerater, SignatureGenerater>();
+            _services.AddScoped<Verifyer, Verifyer>();
+        }
+
+        public void AddMsgRepetHandler()
+        {
+            _services.AddScoped<MessageRepetHandler, MessageRepetHandler>();
+        }
+
+        public void AddMsgParser()
+        {
+            _services.AddScoped<MessageParser, MessageParser>();
+        }
+
+        public void AddMsgProcesser()
+        {
+            _services.AddScoped<MessageProcesser, MessageProcesser>();
+        }
+
+        public void AddMsgHandler()
+        {
+            _services.AddScoped<IClickEvtMessageHandler, DefaultClickEvtMessageHandler>();
+            _services.AddScoped<IImageMessageHandler, DefaultImageMessageHandler>();
+            _services.AddScoped<ILinkMessageHandler, DefaultLinkMessageHandler>();
+            _services.AddScoped<ILocationEvtMessageHandler, DefaultLocationEvtMessageHandler>();
+            _services.AddScoped<ILocationMessageHandler, DefaultLocationMessageHandler>();
+            _services.AddScoped<IScanEvtMessageHandler, DefaultScanEvtMessageHandler>();
+            _services.AddScoped<IScanSubscribeEvtMessageHandler, DefaultScanSubscribeEvtMessageHandler>();
+            _services.AddScoped<IShortVideoMessageHandler, DefaultShortVideoMessageHandler>();
+            _services.AddScoped<ISubscribeEvtMessageHandler, DefaultSubscribeEvtMessageHandler>();
+            _services.AddScoped<ITextMessageHandler, DefaultTextMessageHandler>();
+            _services.AddScoped<IUnsubscribeEvtMessageHandler, DefaultUnsubscribeEvtMessageHandler>();
+            _services.AddScoped<IVideoMessageHandler, DefaultVideoMessageHandler>();
+            _services.AddScoped<IViewEvtMessageHandler, DefaultViewEvtMessageHandler>();
+            _services.AddScoped<IVoiceMessageHandler, DefaultVoiceMessageHandler>();
+        }
+
+        public void AddMsgReply()
+        {
+            _services.AddScoped<IMessageReply<ImageMessage>, ImageMessageReply>();
+            _services.AddScoped<IMessageReply<MusicMessage>, MusicMessageReply>();
+            _services.AddScoped<IMessageReply<NewsMessage>, NewsMessageReply>();
+            _services.AddScoped<IMessageReply<TextMessage>, TextMessageReply>();
+            _services.AddScoped<IMessageReply<VideoMessage>, VideoMessageReply>();
+            _services.AddScoped<IMessageReply<VoiceMessage>, VoiceMessageReply>();
+        }
+
+        public void AddJsApi()
+        {
+            _services.AddScoped<ConfigGenerater, ConfigGenerater>();
+        }
+
+        public void AddBaseSetting()
+        {
+            _services.AddSingleton<BaseSettings, BaseSettings>(s => _options.BaseSetting);
+        }
+
+        public void AddCache()
+        {
+            switch (_options.CacheType)
+            {
+                case CacheType.InMemory:
+                    //InMemory
+                    _services.AddMemoryCache();
+                    _services.AddScoped<ICache, InMemoryCache>();
+                    break;
+                case CacheType.Redis:
+                    //Redis
+                    if (_options.RedisConfig == null)
+                        throw new RedisNotConfiguredExpection("Redis未配置");
+                    _services.AddDistributedRedisCache(opt =>
+                    {
+                        opt.Configuration = $"{_options.RedisConfig.Host}:{_options.RedisConfig.Port}{(string.IsNullOrEmpty(_options.RedisConfig.Password) ? string.Empty : string.Concat(",password=", _options.RedisConfig.Password))}";
+                    });
+                    _services.AddScoped<ICache, RedisCache>();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void AddMsgMiddleware()
+        {
+            switch (_options.MsgMiddlewareType)
+            {
+                case MessageMiddlewareType.Plain:
+                    //明文
+                    _services.AddScoped<IMessageMiddleware, MessageMiddlePlain>();
+                    break;
+                case MessageMiddlewareType.Cipher:
+                    //密文
+                    _services.AddScoped<IMessageMiddleware, MessageMiddleCipher>();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
--- src/Aquarius.Weixin/Aquarius.Weixin/Core/Configuration/DependencyInjection/AquariusWeixinServiceCollectionExtensions.cs
+++ src/Aquarius.Weixin/Aquarius.Weixin/Core/Configuration/DependencyInjection/AquariusWeixinServiceCollectionExtensions.cs
@@ -1,23 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using RestSharp;
 using System;
-using Aquarius.Weixin.Cache;
-using Aquarius.Weixin.Core.Authentication;
 using Aquarius.Weixin.Core.Configuration.DependencyInjection.Options;
-using Aquarius.Weixin.Core.Exceptions;
-using Aquarius.Weixin.Core.InterfaceCaller;
-using Aquarius.Weixin.Core.JsApi;
-using Aquarius.Weixin.Core.MaintainContainer;
-using Aquarius.Weixin.Core.Message;
-using Aquarius.Weixin.Core.Message.Handler;
-using Aquarius.Weixin.Core.Message.Processer;
-using Aquarius.Weixin.Core.Message.Reply;
-using Aquarius.Weixin.Core.Middleware;
-using Aquarius.Weixin.Entity.Configuration;
-using Aquarius.Weixin.Entity.Enums;
-using Aquarius.Weixin.Entity.WeixinMessage;
-using Aquarius.Weixin.Core.Message.Handler.DefaultHandler;
 
 namespace Aquarius.Weixin.Core.Configuration.DependencyInjection
 {
@@ -26,129 +10,30 @@ namespace Aquarius.Weixin.Core.Configuration.DependencyInjection
     /// </summary>
     public static class AquariusWeixinServiceCollectionExtensions
     {
-        private static AquariusWeixinOptions options = new AquariusWeixinOptions()
-        {
-            CacheType = CacheType.InMemory,
-            MsgMiddlewareType = MessageMiddlewareType.Plain,
-            BaseSetting = new BaseSettings()
-            {
-                Debug = true,
-                IsRepetValid = false,
-                AppId = string.Empty,
-                AppSecret = string.Empty,
-                Token = string.Empty,
-                EncodingAESKey = string.Empty,
-                ApiKey = string.Empty,
-                CertPass = string.Empty,
-                CertRoot = string.Empty,
-                MchId = string.Empty
-            },
-            RedisConfig = null
-        };
-
         /// <summary>
         /// 添加 Aquarius.Weixin
         /// </summary>
         /// <param name="services">The services.</param>
         /// <returns></returns>
-        public static IServiceCollection AddAquariusWeixin(this IServiceCollection services)
+        public static IAquariusWeixinBuilder AddAquariusWeixin(this IServiceCollection services)
         {
-            //RestSharp
-            services.AddScoped<IRestClient, RestClient>();
+            IAquariusWeixinBuilder builder = new AquariusWeixinBuilder(services);
 
-            //接口调用
-            services.AddScoped<DisposableMessageInterfaceCaller, DisposableMessageInterfaceCaller>();
-            services.AddScoped<MenuInterfaceCaller, MenuInterfaceCaller>();
-            services.AddScoped<OAuthInterfaceCaller, OAuthInterfaceCaller>();
-            services.AddScoped<TemplateMessageInterfaceCaller, TemplateMessageInterfaceCaller>();
-            services.AddScoped<TicketInterfaceCaller, TicketInterfaceCaller>();
-            services.AddScoped<UserTagManageInterfaceCaller, UserTagManageInterfaceCaller>();
-            services.AddScoped<WxPayInterfaceCaller, WxPayInterfaceCaller>();
+            builder.AddRestSharp();
+            builder.AddInterfaceCallers();
+            builder.AddContainers();
+            builder.AddSignGenerAndVerifyer();
+            builder.AddMsgRepetHandler();
+            builder.AddMsgParser();
+            builder.AddMsgProcesser();
+            builder.AddMsgHandler();
+            builder.AddMsgReply();
+            builder.AddJsApi();
+            builder.AddBaseSetting();
+            builder.AddCache();
+            builder.AddMsgMiddleware();
 
-            //容器
-            services.AddScoped<AccessTokenContainer, AccessTokenContainer>();
-            services.AddScoped<AuthorizationContainer, AuthorizationContainer>();
-            services.AddScoped<TicketContainer, TicketContainer>();
-
-            //签名与验证
-            services.AddScoped<SignatureGenerater, SignatureGenerater>();
-            services.AddScoped<Verifyer, Verifyer>();
-
-            //消息
-            //消息重复处理
-            services.AddScoped<MessageRepetHandler, MessageRepetHandler>();
-            //消息解析器
-            services.AddScoped<MessageParser, MessageParser>();
-            //消息处理器
-            services.AddScoped<MessageProcesser, MessageProcesser>();
-            //消息处理方法
-            services.AddScoped<IClickEvtMessageHandler, DefaultClickEvtMessageHandler>();
-            services.AddScoped<IImageMessageHandler, DefaultImageMessageHandler>();
-            services.AddScoped<ILinkMessageHandler, DefaultLinkMessageHandler>();
-            services.AddScoped<ILocationEvtMessageHandler, DefaultLocationEvtMessageHandler>();
-            services.AddScoped<ILocationMessageHandler, DefaultLocationMessageHandler>();
-            services.AddScoped<IScanEvtMessageHandler, DefaultScanEvtMessageHandler>();
-            services.AddScoped<IScanSubscribeEvtMessageHandler, DefaultScanSubscribeEvtMessageHandler>();
-            services.AddScoped<IShortVideoMessageHandler, DefaultShortVideoMessageHandler>();
-            services.AddScoped<ISubscribeEvtMessageHandler, DefaultSubscribeEvtMessageHandler>();
-            services.AddScoped<ITextMessageHandler, DefaultTextMessageHandler>();
-            services.AddScoped<IUnsubscribeEvtMessageHandler, DefaultUnsubscribeEvtMessageHandler>();
-            services.AddScoped<IVideoMessageHandler, DefaultVideoMessageHandler>();
-            services.AddScoped<IViewEvtMessageHandler, DefaultViewEvtMessageHandler>();
-            services.AddScoped<IVoiceMessageHandler, DefaultVoiceMessageHandler>();
-            //消息回复
-            services.AddScoped<IMessageReply<ImageMessage>, ImageMessageReply>();
-            services.AddScoped<IMessageReply<MusicMessage>, MusicMessageReply>();
-            services.AddScoped<IMessageReply<NewsMessage>, NewsMessageReply>();
-            services.AddScoped<IMessageReply<TextMessage>, TextMessageReply>();
-            services.AddScoped<IMessageReply<VideoMessage>, VideoMessageReply>();
-            services.AddScoped<IMessageReply<VoiceMessage>, VoiceMessageReply>();
-
-            //JS-API
-            services.AddScoped<ConfigGenerater, ConfigGenerater>();
-
-            //缓存
-            switch (options.CacheType)
-            {
-                case CacheType.InMemory:
-                    //InMemory
-                    services.AddMemoryCache();
-                    services.AddScoped<ICache, InMemoryCache>();
-                    break;
-                case CacheType.Redis:
-                    //Redis
-                    if (options.RedisConfig == null)
-                        throw new RedisNotConfiguredExpection("Redis未配置");
-                    services.AddDistributedRedisCache(opt =>
-                    {
-                        opt.Configuration = $"{options.RedisConfig.Host}:{options.RedisConfig.Port}{(string.IsNullOrEmpty(options.RedisConfig.Password) ? string.Empty : string.Concat(",password=", options.RedisConfig.Password))}";
-                    });
-                    services.AddScoped<ICache, RedisCache>();
-                    break;
-                default:
-                    break;
-            }
-
-            //微信设置
-            services.AddScoped<BaseSettings, BaseSettings>(provider =>
-                options.BaseSetting);
-
-            //消息中间件
-            switch(options.MsgMiddlewareType)
-            {
-                case MessageMiddlewareType.Plain:
-                    //明文
-                    services.AddScoped<IMessageMiddleware, MessageMiddlePlain>();
-                    break;
-                case MessageMiddlewareType.Cipher:
-                    //密文
-                    services.AddScoped<IMessageMiddleware, MessageMiddleCipher>();
-                    break;
-                default:
-                    break;
-            }
-
-            return services;
+            return builder;
         }
 
         /// <summary>
@@ -157,10 +42,10 @@ namespace Aquarius.Weixin.Core.Configuration.DependencyInjection
         /// <param name="services">The services.</param>
         /// <param name="setupAction">The setup action.</param>
         /// <returns></returns>
-        public static IServiceCollection AddAquariusWeixin(this IServiceCollection services, Action<AquariusWeixinOptions> setupAction)
+        public static IAquariusWeixinBuilder AddAquariusWeixin(this IServiceCollection services, Action<AquariusWeixinOptions> setupAction)
         {
-            setupAction.Invoke(options);
-            return services.AddAquariusWeixin();
+            services.Configure(setupAction);
+            return AddAquariusWeixin(services);
         }
 
         /// <summary>
@@ -169,9 +54,9 @@ namespace Aquarius.Weixin.Core.Configuration.DependencyInjection
         /// <param name="services">The services.</param>
         /// <param name="configuration">The configuration.</param>
         /// <returns></returns>
-        public static IServiceCollection AddAquariusWeixin(this IServiceCollection services, IConfiguration configuration)
+        public static IAquariusWeixinBuilder AddAquariusWeixin(this IServiceCollection services, IConfiguration configuration)
         {
-            options = configuration.Get<AquariusWeixinOptions>();
+            services.Configure<AquariusWeixinOptions>(configuration);
             return services.AddAquariusWeixin();
         }
     }
