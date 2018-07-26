25be5e35145748f1181db1ce455e0ca94ac26ee0
Weidaicheng <wdcdavyc@outlook.com>
2018-07-26 15:26:57 +0800

    提取微信接口链接到Uris静态类

---
+++ src/Aquarius.Weixin/Aquarius.Weixin/Entity/Enums/Uris.cs
@@ -0,0 +1,18 @@
+﻿namespace Aquarius.Weixin.Entity.Enums
+{
+    /// <summary>
+    /// Uri
+    /// </summary>
+    public static class Uris
+    {
+        /// <summary>
+        /// 微信Uri
+        /// </summary>
+        public const string WxUri = "https://api.weixin.qq.com";
+
+        /// <summary>
+        /// 微信支付Uri
+        /// </summary>
+        public const string WxPayUri = "https://api.mch.weixin.qq.com";
+    }
+}
